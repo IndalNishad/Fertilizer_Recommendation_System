fb2f177c698f423308967876f93669a01c671221
INDAL NISHAD <109014444+IndalNishad@users.noreply.github.com>
2025-07-23 15:40:20 +0530

    Update FertilizerFormModal.js

--- fertilizer-frontend/src/components/FertilizerFormModal.js
+++ fertilizer-frontend/src/components/FertilizerFormModal.js
@@ -5,7 +5,7 @@ import React, { useState, useEffect, useRef } from "react";
 import "./FertilizerFormModal.css";
 import axios from "axios";
 
-const API_BASE = process.env.REACT_APP_API_URL || "http://localhost:5000";
+const API_BASE = process.env.REACT_APP_API_URL || "https://fertilizer-recommendation-system-rova.onrender.com";
 
 const FertilizerFormModal = ({ isOpen, onClose }) => {
   const [formData, setFormData] = useState({
